615ad621cdb6dc1decf843eeae7164dcc98215da
Timeo tregarot <timeo.tregarot@epitech.eu>
2025-11-23 15:55:56 +0100

    feat(PoC/ECS/System) : add system class

--- src/gameEngine/ECS/System/System.cpp
+++ src/gameEngine/ECS/System/System.cpp
@@ -7,22 +7,32 @@
 
 #include "System.hpp"
 
-void System::onCreate()
+void System::addEntity(Entity e)
 {
+    auto id = static_cast<std::size_t>(e);
+    for (auto const& ent : _entities) {
+        if (static_cast<std::size_t>(ent) == id)
+            return;
+    }
+
+    _entities.push_back(e);
+
+    if (!_running) {
+        _running = true;
+        onStartRunning();
+    }
 }
 
-void System::onStartRunning()
+void System::removeEntity(Entity const& e)
 {
-}
+    auto id = static_cast<std::size_t>(e);
 
-void System::onUpdate()
-{
-}
+    _entities.remove_if([id](Entity const& candidate) {
+        return static_cast<std::size_t>(candidate) == id;
+    });
 
-void System::onStopRunning()
-{
-}
-
-void System::onDestroy()
-{
+    if (_entities.empty() && _running) {
+        _running = false;
+        onStopRunning();
+    }
 }
