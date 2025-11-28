41e299b1294a2424f09754a16d4cd051ad26d1cf
Aymeric <aymeric.jouannet-mimy@epitech.eu>
2025-11-28 12:45:49 +0100

    refactor(MVP/Client): apply the cases for ComponentManager class and the doc

--- src/gameEngine/ECS/Component/ComponentManager.hpp
+++ src/gameEngine/ECS/Component/ComponentManager.hpp
@@ -16,14 +16,14 @@ template <typename Component>
 
 class ComponentManager {
     public :
-        using value_type = std::optional<Component>; // optional component type
-        using reference_type = value_type &;
-        using const_reference_type = value_type const&;
-        using container_t = std::vector<value_type>; // optionally add your allocator template here.
-        using size_type = typename container_t::size_type;
+        using valueType = std::optional<Component>; // optional component type
+        using referenceType = valueType &;
+        using constReferenceType = valueType const&;
+        using containerT = std::vector<valueType>; // optionally add your allocator template here.
+        using sizeType = typename containerT::sizeType;
 
-        using iterator = typename container_t::iterator;
-        using const_iterator = typename container_t::const_iterator;
+        using iterator = typename containerT::iterator;
+        using constIterator = typename containerT::constIterator;
 
     public :
         ComponentManager () = default; // You can add more constructors .
@@ -36,74 +36,74 @@ class ComponentManager {
         ComponentManager & operator=(ComponentManager &&) noexcept = default; // move assignment operator
 
         // Access
-        reference_type operator[](size_t idx) {
+        referenceType operator[](size_t idx) {
             return this->_data[idx];
         }
 
-        const_reference_type operator[](size_t idx) const {
+        constReferenceType operator[](size_t idx) const {
             return this->_data[idx];
         }
 
         iterator begin () {
             return this->_data.begin();
         }
-        const_iterator begin () const {
+        constIterator begin () const {
             return this->_data.begin();
         }
-        const_iterator cbegin () const {
+        constIterator cbegin () const {
             return this->_data.cbegin();
         }
 
         iterator end () {
             return this->_data.end();
         }
-        const_iterator end () const {
+        constIterator end () const {
             return this->_data.end();
         }
-        const_iterator cend () const {
+        constIterator cend () const {
             return this->_data.cend();
         }
 
-        size_type size () const {
+        sizeType size () const {
             return this->_data.size();
         }
 
         // sert à garantir que l’index demandé existe dans le vecteur avant d’y écrire
-        void ensure_size(size_type pos) {
+        void ensureSize(sizeType pos) {
             if (pos >= this->_data.size())
                 this->_data.resize(pos + 1);
         }
 
-        reference_type insert_at (size_type pos, Component const& c) {
-            ensure_size(pos); // pour agrandir
+        referenceType insertAt (sizeType pos, Component const& c) {
+            ensureSize(pos); // pour agrandir
             this->_data[pos] = c;
             return this->_data[pos]; // rajouter dans le vector
         }
 
-        reference_type insert_at (size_type pos, Component &&c) {
-            ensure_size(pos); // pour agrandir
+        referenceType insertAt (sizeType pos, Component &&c) {
+            ensureSize(pos); // pour agrandir
             this->_data[pos] = std::move(c);
             return this->_data[pos]; // rajouter dans le vector
         }
 
         template <class... Params>
-        reference_type emplace_at (size_type pos, Params &&... args) {
-            ensure_size(pos);
+        referenceType emplaceAt (sizeType pos, Params &&... args) {
+            ensureSize(pos);
             this->_data[pos].reset();
             this->_data[pos].emplace(std::forward<Params>(args)...);
             return this->_data[pos];
         }
 
-        void erase (size_type pos) {
+        void erase (sizeType pos) {
             if (this->_data[pos])
                 this->_data[pos].reset();
         }
-        size_type get_index (value_type const& v) const {
+        sizeType getIndex (valueType const& v) const {
             return &v - this->_data.data();
         }
 
     private :
-        container_t _data;
+        containerT _data;
 };
 
 #endif /* !COMPONENTMANAGER_HPP_ */
