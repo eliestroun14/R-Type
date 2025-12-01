5dadc86e5a5b18bb4fa09501c62f9ac0c07bd792
Aymeric <aymeric.jouannet-mimy@epitech.eu>
2025-12-01 13:51:28 +0100

    feat(MVP/Client): add RTypeClient class, not implemented yet

---
+++ src/client/RTypeClient.hpp
@@ -0,0 +1,40 @@
+/*
+** EPITECH PROJECT, 2025
+** mirror_rtype
+** File description:
+** RTypeClient
+*/
+
+#ifndef RTYPECLIENT_HPP_
+#define RTYPECLIENT_HPP_
+
+#include <cstdint>
+#include <sys/socket.h>
+#include <SFML/Graphics.hpp>
+
+class RTypeClient {
+    public:
+        RTypeClient();
+        ~RTypeClient();
+
+        void init(const char* serverIp, uint16_t port);
+
+        bool connect(const char* serverIp, uint16_t port);
+
+        void sendInput(uint16_t inputFlags);
+        void sendHeartbeat();
+
+        void processWorldSnapshot();
+
+        void run();
+
+    private:
+        int _sockfd;
+        struct sockaddr_in *_serverAddr;
+        uint32_t _sequenceNumber;
+        uint32_t _playerId;
+        bool _connected;
+        sf::Clock _clock;
+};
+
+#endif /* !RTYPECLIENT_HPP_ */
