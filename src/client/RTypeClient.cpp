120555ea2c853b29b72bf66a3055c5082d865632
Aymeric <aymeric.jouannet-mimy@epitech.eu>
2025-12-02 12:39:29 +0100

    feat(MVP/Client): update RTypeClient method implement connect method added and (not fully) implemented waitForAccept method

--- src/client/RTypeClient.cpp
+++ src/client/RTypeClient.cpp
@@ -6,6 +6,10 @@
 */
 
 #include "RTypeClient.hpp"
+#include <string.h>
+#include <arpa/inet.h>
+#include <src/utils/client/ClientUtils.hpp>
+#include <thread>
 
 RTypeClient::RTypeClient()
 {
@@ -19,8 +23,67 @@ void RTypeClient::init(const char *serverIp, uint16_t port)
 {
 }
 
-bool RTypeClient::connect(const char *serverIp, uint16_t port)
+bool RTypeClient::waitForAccept(protocol::ServerAccept *serverAccept)
 {
+    uint32_t startTime = getCurrentTimeMs();
+    const uint32_t timeout = TIMEOUT;
+
+    while (true)
+    {
+        if (serverAccept->header.packet_type ==
+            static_cast<uint8_t>(protocol::PacketTypes::TYPE_SERVER_ACCEPT))
+            return true;
+
+        uint32_t currentTime = getCurrentTimeMs();
+        if (currentTime - startTime >= timeout)
+            return false;
+
+        // mini pause pour pas full cosommer le CPU, à voir si le garde, si ça met trop de latence on enlève
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+
+    return false;
+}
+
+bool RTypeClient::connect(const char *serverIp, uint16_t port, std::string playerName)
+{
+    // Create UDP socket
+    this->_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (this->_sockfd)
+        return false;
+
+    // Setup server address
+    memset(&this->_serverAddr, 0, sizeof(this->_serverAddr));
+    this->_serverAddr.sin_family = AF_INET;
+    this->_serverAddr.sin_port = htons(port);
+    inet_pton(AF_INET, serverIp, &this->_serverAddr.sin_addr);
+
+    // Send CLIENT_CONNECT
+    protocol::ClientConnect connectPacket;
+    connectPacket.header.packet_type = static_cast<uint8_t>(protocol::PacketTypes::TYPE_CLIENT_CONNECT);
+    connectPacket.header.flags = 0;
+    connectPacket.header.sequence_number = this->_sequenceNumber++;
+    connectPacket.header.timestamp = getCurrentTimeMs();
+    connectPacket.protocol_version = 1;
+    strncpy(connectPacket.player_name, playerName.c_str(), 31);
+    connectPacket.client_id = generateClientId();
+
+    //Serialize and send
+    uint8_t buffer[256];
+    // serializeClientConnect(&connectPacket, buffer); TODO: faire serializeClientConnect()
+    sendto(this->_sockfd, buffer, sizeof(protocol::ClientConnect), 0,
+        (struct sockaddr*)&this->_serverAddr, sizeof(this->_serverAddr));
+
+
+    // Wait for SERVER_ACCEPT
+    protocol::ServerAccept serverAccept;
+
+    if (waitForAccept(&serverAccept)) {
+        this->_connected = true;
+        this->_playerId = serverAccept.assigned_player_id;
+        return true;
+    }
+
     return false;
 }
 
@@ -30,11 +93,11 @@ void RTypeClient::sendInput(uint16_t inputFlags)
 
 void RTypeClient::sendHeartbeat()
 {
-    protocol::HeartBeat *heartbeat;
+    protocol::HeartBeat heartbeat;
 
-    heartbeat->header.packet_type = static_cast<uint8_t>(protocol::PacketTypes::TYPE_HEARTBEAT);
-    heartbeat->player_id = this->_playerId;
-    send(this->_sockfd, heartbeat, sizeof(protocol::HeartBeat), NULL);
+    heartbeat.header.packet_type = static_cast<uint8_t>(protocol::PacketTypes::TYPE_HEARTBEAT);
+    heartbeat.player_id = this->_playerId;
+    send(this->_sockfd, &heartbeat, sizeof(protocol::HeartBeat), NULL);
 }
 
 void RTypeClient::processWorldSnapshot()
--- src/client/RTypeClient.hpp
+++ src/client/RTypeClient.hpp
@@ -11,7 +11,10 @@
 #include <cstdint>
 #include <sys/socket.h>
 #include <SFML/Graphics.hpp>
+#include <netinet/in.h>
 #include "src/server/protocol/protocol.hpp"
+
+#define TIMEOUT 15000
 
 class RTypeClient {
     public:
@@ -20,7 +23,8 @@ class RTypeClient {
 
         void init(const char* serverIp, uint16_t port);
 
-        bool connect(const char* serverIp, uint16_t port);
+        bool waitForAccept(protocol::ServerAccept *serverAccept);
+        bool connect(const char* serverIp, uint16_t port, std::string playerName);
 
         void sendInput(uint16_t inputFlags);
         void sendHeartbeat();
@@ -31,13 +35,12 @@ class RTypeClient {
 
     private:
         int _sockfd;
-        struct sockaddr_in *_serverAddr;
+        struct sockaddr_in _serverAddr;
         uint32_t _sequenceNumber;
         uint32_t _playerId;
         bool _connected;
-        sf::Clock _clock;
 
-        uint32_t _playerId;
+        sf::Clock _clock;
 };
 
 #endif /* !RTYPECLIENT_HPP_ */
