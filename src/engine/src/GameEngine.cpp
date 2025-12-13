63d23764afc90d8c072ecb82bf72eb52d6fb1741
Aymeric <aymeric.jouannet-mimy@epitech.eu>
2025-12-13 15:20:31 +0100

    feat(MVP/Coordinator/GameLoop): add stars background to assets and to the game

--- src/engine/src/GameEngine.cpp
+++ src/engine/src/GameEngine.cpp
@@ -47,6 +47,7 @@ namespace gameEngine {
         _coordinator->setSystemSignature<AnimationSystem, Animation, Sprite>();
 
         Entity player = _coordinator->createEntity("Player");
+        Entity background = _coordinator->createEntity("Stars Background");
         
         _coordinator->addComponent<Sprite>(player, Sprite(PLAYER_1, 1, sf::IntRect(0, 0, 33, 15)));
         _coordinator->addComponent<Transform>(player, Transform(100.f, 150.f, 0.f, 5.0f));
@@ -55,6 +56,9 @@ namespace gameEngine {
         // Animation: frameWidth=33, frameHeight=15, currentFrame=2 (neutral), elapsedTime=0, frameDuration=0.1s
         // startFrame=2, endFrame=2 (neutral frame), loop=true
         _coordinator->addComponent<Animation>(player, Animation(33, 15, 2, 0.f, 0.1f, 2, 2, true));
+
+        _coordinator->addComponent<Sprite>(background, Sprite(STARS_BG, 0, sf::IntRect(0, 0, 755, 448)));
+        _coordinator->addComponent<Transform>(background, Transform(0.f, 0.f, 0.f, 5.0f));
 
         _coordinator->onCreateSystems();
     }
