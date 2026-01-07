f407c4f22ebdd32229d2d013329849a2be88f060
Aymeric <aymeric.jouannet-mimy@epitech.eu>
2026-01-07 21:11:35 +0100

    feat(feat/FP/AudioManager): add all audio assets in Assets && Protocol

--- src/common/include/common/constants/render/Assets.hpp
+++ src/common/include/common/constants/render/Assets.hpp
@@ -9,7 +9,7 @@
 #define ASSETS_HPP_
 
 #define NUMBER_ASSETS 17
-#define NUMBER_AUDIO_ASSETS 1
+#define NUMBER_AUDIO_ASSETS 25
 
 
 #include <array>
@@ -88,14 +88,89 @@ static const std::array<std::string, NUMBER_ASSETS> pathAssets = {
 
 
 enum AudioAssets {
+    // WEAPONS
+    SFX_SHOOT_BASIC,
+    SFX_SHOOT_CHARGED,
+    SFX_SHOOT_LASER,
+
     // EXPLOSIONS
-    EXPLOSION_AUDIO
+    SFX_EXPLOSION_SMALL_1,
+    SFX_EXPLOSION_SMALL_2,
+    SFX_EXPLOSION_LARGE_1,
+    SFX_EXPLOSION_LARGE_2,
+
+    // POWERUPS
+    SFX_POWERUP_COLLECT_1,
+    SFX_POWERUP_COLLECT_2,
+
+    // PLAYER
+    SFX_PLAYER_HIT,
+    SFX_PLAYER_DEATH_1,
+    SFX_PLAYER_DEATH_2,
+    SFX_PLAYER_DEATH_3,
+
+    // FORCE
+    SFX_FORCE_ATTACH,
+    SFX_FORCE_DETACH,
+
+    // BOSS
+    SFX_BOSS_ROAR,
+
+    // UI
+    SFX_MENU_SELECT,
+    SFX_MENU_ALERT,
+
+    // MUSIC
+    MAIN_MENU_MUSIC,
+    FIRST_LEVEL_MUSIC,
+    SECOND_LEVEL_MUSIC,
+    THIRD_LEVEL_MUSIC,
+    FOURTH_LEVEL_MUSIC,
+    VICTORY_MUSIC,
+    DEFEAT_MUSIC,
 };
 
 static const std::array<std::string, NUMBER_AUDIO_ASSETS> pathAudioAssets = {
-    // EXPLOSIONS
-    "../assets/explosions/explosion.png",
+    // WEAPONS
+    "../assets/audio/weapons/basic.wav",
+    "../assets/audio/weapons/charged.wav",
+    "../assets/audio/weapons/laser.wav",
 
+    // EXPLOSIONS
+    "../assets/audio/explosions/smallExplosion1.wav",
+    "../assets/audio/explosions/smallExplosion2.wav",
+    "../assets/audio/explosions/largeExplosion1.wav",
+    "../assets/audio/explosions/largeExplosion2.wav",
+
+    // POWERUPS
+    "../assets/audio/powerups/collectPowerup1.wav",
+    "../assets/audio/powerups/collectPowerup2.wav",
+
+    // PLAYER
+    "../assets/audio/player/playerHit.wav",
+    "../assets/audio/player/playerDeath1.wav",
+    "../assets/audio/player/playerDeath2.wav",
+    "../assets/audio/player/playerDeath3.wav",
+
+    // FORCE
+    "../assets/audio/force/forceAttach.wav",
+    "../assets/audio/force/forceDetach.wav",
+
+    // BOSS
+    "../assets/audio/boss/bossRoar.wav",
+
+    // UI
+    "../assets/audio/ui/select.wav",
+    "../assets/audio/ui/alert.wav",
+
+    // MUSIC
+    "../assets/audio/music/mainMenuMusic.wav",
+    "../assets/audio/music/firstLevelMusic.wav",
+    "../assets/audio/music/secondLevelMusic.wav",
+    "../assets/audio/music/thirdLevelMusic.wav",
+    "../assets/audio/music/fourthLevelMusic.wav",
+    "../assets/audio/music/victoryMusic.wav",
+    "../assets/audio/music/defeatMusic.wav",
 
 };
 
