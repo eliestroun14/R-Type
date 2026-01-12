7b3e9d95eb531c65ffee35156d29dd16ac1eed2b
Aymeric <aymeric.jouannet-mimy@epitech.eu>
2026-01-12 19:02:28 +0100

    feat(ClientMenu): add ClientMenu .hpp && .cpp

---
+++ src/client/include/client/menu/ClientMenu.hpp
@@ -0,0 +1,37 @@
+/*
+** EPITECH PROJECT, 2026
+** mirror_rtype
+** File description:
+** ClientMenu
+*/
+
+#ifndef CLIENTMENU_HPP_
+#define CLIENTMENU_HPP_
+
+#include <game/Game.hpp>
+
+class ClientMenu {
+
+    public:
+        ClientMenu();
+        ~ClientMenu();
+
+
+        /**
+         * @brief Method that create every entity for the main menu.
+         */
+        void createMainMenu();
+
+        /**
+         * @brief Method that create every entity for the option menu.
+         */
+        void createOptionMenu();
+
+    private:
+        bool _isConnected;
+        std::unique_ptr<Game> _game;
+
+
+};
+
+#endif /* !CLIENTMENU_HPP_ */
