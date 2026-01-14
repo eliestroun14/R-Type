f8f1a84f64422b857b021640d9bdee5864019a72
Aymeric <aymeric.jouannet-mimy@epitech.eu>
2026-01-14 10:54:30 +0100

    feat(ClientMenu): update the ClientMenu

--- src/client/include/client/menu/ClientMenu.hpp
+++ src/client/include/client/menu/ClientMenu.hpp
@@ -8,30 +8,45 @@
 #ifndef CLIENTMENU_HPP_
 #define CLIENTMENU_HPP_
 
-#include <game/Game.hpp>
+#include <engine/GameEngine.hpp>
 #include <game/utils/ClientUtils.hpp>
+#include <game/menu/IMenu.hpp>
+#include <queue>
 
-class ClientMenu {
+class ClientMenu : public IMenu {
 
     public:
-        ClientMenu();
+        ClientMenu(std::shared_ptr<gameEngine::GameEngine> engine);
         ~ClientMenu();
 
+        template<typename T>
+        void addMenuEntity(T entityOrList) {
+            if constexpr (std::is_same_v<T, Entity>)
+                _menuEntities.push_back(entityOrList);
+            else
+                _menuEntities.insert(_menuEntities.end(), entityOrList.begin(), entityOrList.end());
+        }
+
+        void update() override;
 
         /**
          * @brief Method that create every entity for the main menu.
          */
-        void createMainMenu();
+        void createMainMenu() override;
 
         /**
          * @brief Method that create every entity for the option menu.
          */
-        void createOptionMenu();
+        void createOptionMenu() override;
+
+
+        void clearMenuEntities();
 
     private:
         bool _isConnected;
-        std::unique_ptr<Game> _game;
-
+        std::shared_ptr<gameEngine::GameEngine> _engine;
+        std::vector<Entity> _menuEntities;
+        std::queue<std::function<void()>> _pendingActions;
 
 };
 
--- src/client/src/menu/ClientMenu.cpp
+++ src/client/src/menu/ClientMenu.cpp
@@ -7,11 +7,52 @@
 
 #include <client/menu/ClientMenu.hpp>
 
-ClientMenu::ClientMenu()
-{
-
-}
+ClientMenu::ClientMenu(std::shared_ptr<gameEngine::GameEngine> engine)
+    : _engine(engine)
+{}
 
 ClientMenu::~ClientMenu()
 {
+}
+
+void ClientMenu::update() {
+    while (!_pendingActions.empty()) {
+        _pendingActions.front()();
+        _pendingActions.pop();
+    }
+}
+
+void ClientMenu::createMainMenu()
+{
+    // BACKGROUND
+    addMenuEntity(createImage(*this->_engine, Assets::GAME_BG,
+        sf::Vector2f(0, 0), MAIN_MENU_BG_ROTATION, MAIN_MENU_BG_SCALE,
+        sf::IntRect(0, 0, MAIN_MENU_BG_SPRITE_WIDTH, MAIN_MENU_BG_SPRITE_HEIGHT),
+        ZIndex::IS_BACKGROUND));
+
+
+    
+
+    // BUTTONSs
+    addMenuEntity(createButton(*_engine, "OPTIONS", 30, sf::Color::White, {400, 500}, 1.0f, sf::IntRect(0, 0, DEFAULT_BUTTON_SPRITE_WIDTH, DEFAULT_BUTTON_SPRITE_HEIGHT),
+        DEFAULT_NONE_BUTTON, DEFAULT_HOVER_BUTTON, DEFAULT_CLICKED_BUTTON,
+        [this]() {
+            this->_pendingActions.push([this]() {
+                this->clearMenuEntities();
+                this->createOptionMenu();
+            });
+        }
+    ));
+}
+
+void ClientMenu::createOptionMenu()
+{
+    std::cout << "ClientMenu Option Menu created" << std::endl;
+}
+
+void ClientMenu::clearMenuEntities()
+{
+    for (auto& entity : _menuEntities)
+        _engine->destroyEntity(entity);
+    _menuEntities.clear();
 }
