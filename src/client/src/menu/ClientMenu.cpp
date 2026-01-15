b015f7c1a7c285a2baac544eb93bd681cebdcd9f
Aymeric <aymeric.jouannet-mimy@epitech.eu>
2026-01-15 18:21:46 +0100

    feat(ClientMenu): start the keybind menu, add button (no function inside)

--- src/client/src/menu/ClientMenu.cpp
+++ src/client/src/menu/ClientMenu.cpp
@@ -19,6 +19,12 @@ ClientMenu::~ClientMenu()
 }
 
 void ClientMenu::update() {
+
+    // std::cout << "#################### DEBUG ##################" << std::endl;
+    // std::cout << "###################" << this->_menuEntities.size() << " ###################" << std::endl;
+    // std::cout << "#################### DEBUG ##################" << std::endl;
+
+
     while (!_pendingActions.empty()) {
         _pendingActions.front()();
         _pendingActions.pop();
@@ -223,13 +229,27 @@ void ClientMenu::createKeybindsMenu()
     // TEXT
     addMenuEntity(createText(*this->_engine, "KEYBINDS", 85, sf::Color::White, {485, 210}, 0, 1.5f));
 
+    // TEXTS DEPLACEMENT
+    addMenuEntity(createText(*this->_engine, "MOVEMENT:", 35, sf::Color::White, {200, 350}, 0, 1.5f));
+    addMenuEntity(createText(*this->_engine, "UP   " , 25, sf::Color::White, {120, 450}, 0, 1.5f));
+    addMenuEntity(createText(*this->_engine, "DOWN ", 25, sf::Color::White, {120, 550}, 0, 1.5f));
+    addMenuEntity(createText(*this->_engine, "LEFT ", 25, sf::Color::White, {120, 650}, 0, 1.5f));
+    addMenuEntity(createText(*this->_engine, "RIGHT", 25, sf::Color::White, {120, 750}, 0, 1.5f));
+
+    // TEXTS DEPLACEMENT
+    // addMenuEntity(createText(*this->_engine, "SHOOT", 25, sf::Color::White, {200, 750}, 0, 1.5f));
+    // addMenuEntity(createText(*this->_engine, "SWITCH WEAPON", 25, sf::Color::White, {700, 750}, 0, 1.5f));
+    // addMenuEntity(createText(*this->_engine, "USE POWERUP", 25, sf::Color::White, {250, 950}, 0, 1.5f));
+    // addMenuEntity(createText(*this->_engine, "SPECIAL", 25, sf::Color::White, {700, 950}, 0, 1.5f));
+
+
     // ANIMATED IMAGES
     Animation planetAnimation(OPTION_MENU_PLANET_SPRITE_WIDTH, OPTION_MENU_PLANET_SPRITE_HEIGHT, OPTION_MENU_PLANET_ANIMATION_CURRENT,
         OPTION_MENU_PLANET_ANIMATION_ELAPSED_TIME, OPTION_MENU_PLANET_ANIMATION_DURATION, OPTION_MENU_PLANET_ANIMATION_START,
         OPTION_MENU_PLANET_ANIMATION_END, OPTION_MENU_PLANET_ANIMATION_LOOPING);
 
     addMenuEntity(createAnimatedImage(*this->_engine, Assets::KEYBINDS_MENU_PLANET, planetAnimation, {-200, -1300},
-        0, OPTION_MENU_PLANET_SPRITE_SCALE, sf::IntRect(0, 0, OPTION_MENU_PLANET_SPRITE_WIDTH, OPTION_MENU_PLANET_SPRITE_HEIGHT), ZIndex::IS_BACKGROUND));
+        0, OPTION_MENU_PLANET_SPRITE_SCALE, sf::IntRect(0, 0, OPTION_MENU_PLANET_SPRITE_WIDTH, OPTION_MENU_PLANET_SPRITE_HEIGHT), ZIndex::IS_GAME));
 
     // BUTTONSs
     addMenuEntity(createButton(*_engine, "back", 0, sf::Color::White, {30, 30}, BACK_BUTTON_SCALE,
@@ -242,6 +262,50 @@ void ClientMenu::createKeybindsMenu()
             });
         }
     ));
+
+
+    // DEPLACEMENT BUTTON KEYBINDS
+    addMenuEntity(createButton(*_engine, "UP", 20, sf::Color::White, {230, 400}, DEFAULT_BUTTON_MID_SCALE,
+        sf::IntRect(0, 0, DEFAULT_BUTTON_SPRITE_WIDTH, DEFAULT_BUTTON_SPRITE_HEIGHT),
+        DEFAULT_NONE_BUTTON, DEFAULT_HOVER_BUTTON, DEFAULT_CLICKED_BUTTON,
+        [this]() {
+            this->_pendingActions.push([this]() {
+                //TODO: do something
+            });
+        }
+    ));
+
+    addMenuEntity(createButton(*_engine, "DOWN", 20, sf::Color::White, {230, 500}, DEFAULT_BUTTON_MID_SCALE,
+        sf::IntRect(0, 0, DEFAULT_BUTTON_SPRITE_WIDTH, DEFAULT_BUTTON_SPRITE_HEIGHT),
+        DEFAULT_NONE_BUTTON, DEFAULT_HOVER_BUTTON, DEFAULT_CLICKED_BUTTON,
+        [this]() {
+            this->_pendingActions.push([this]() {
+                //TODO: do something
+            });
+        }
+    ));
+
+    addMenuEntity(createButton(*_engine, "LEFT", 20, sf::Color::White, {230, 600}, DEFAULT_BUTTON_MID_SCALE,
+        sf::IntRect(0, 0, DEFAULT_BUTTON_SPRITE_WIDTH, DEFAULT_BUTTON_SPRITE_HEIGHT),
+        DEFAULT_NONE_BUTTON, DEFAULT_HOVER_BUTTON, DEFAULT_CLICKED_BUTTON,
+        [this]() {
+            this->_pendingActions.push([this]() {
+                //TODO: do something
+            });
+        }
+    ));
+
+    addMenuEntity(createButton(*_engine, "RIGHT", 20, sf::Color::White, {230, 700}, DEFAULT_BUTTON_MID_SCALE,
+        sf::IntRect(0, 0, DEFAULT_BUTTON_SPRITE_WIDTH, DEFAULT_BUTTON_SPRITE_HEIGHT),
+        DEFAULT_NONE_BUTTON, DEFAULT_HOVER_BUTTON, DEFAULT_CLICKED_BUTTON,
+        [this]() {
+            this->_pendingActions.push([this]() {
+                //TODO: do something
+            });
+        }
+    ));
+
+
 }
 
 
